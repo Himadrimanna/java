0e2ad57b30bcf029d7bc82ecc6c2fc3f3db51c56
Himadrimanna <himadrimanna792@gmail.com>
2024-05-03 22:07:45 +0530

    Sorted

---
+++ Sorted.java
@@ -0,0 +1,15 @@
+
+public class Sorted {
+	public static void main(String[] args) {
+		int[] arr = {1,2,8,4,5};
+		System.out.println(sorted(arr, 0));
+	}
+
+	static boolean sorted(int[] arr, int index){
+		if(index == arr.length-1){
+			return true;
+		}
+
+		return arr[index]<arr[index+1] && sorted(arr, index+1);
+	}
+}
