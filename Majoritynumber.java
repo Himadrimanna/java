3845cbce3a67505d1fc2d1d25c57c2047a88ef5c
HIMADRI MANNA <himadrimanna792@gmail.com>
2024-05-09 22:45:57 +0530

    Update Majoritynumber.java

--- Majoritynumber.java
+++ Majoritynumber.java
@@ -1,3 +1,7 @@
+/*Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.*/
+
+
 public class Majoritynumber {
 	public static void main(String[] args) {
 		int[] arr = {2,2,1,1,1,2,2};
